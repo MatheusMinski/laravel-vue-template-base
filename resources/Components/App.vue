943beb4e2540b9ffaad8fb8767e72e364e17e979
/minski <minskisantos@hotmail.com>
2023-05-23 20:16:36 -0300

    chore(chore): views without any css framework

---
+++ resources/Components/App.vue
@@ -0,0 +1,10 @@
+<template>
+    <h1 class="mt-5">Vue working!</h1>
+    <button class="rounded-full">test</button>
+</template>
+
+<script>
+export default {
+    name: 'App'
+}
+</script>
---
+++ resources/views/app.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+</head>
+
+<body id="app">
+
+
+
+    @vite('resources/js/app.js')
+</body>
+
+</html>
