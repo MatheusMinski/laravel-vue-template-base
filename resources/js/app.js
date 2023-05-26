73abb7bbd3cd7dd5e97d58448af5b141409b4402
/minski <minskisantos@hotmail.com>
2023-05-25 21:42:27 -0300

    feat(chore): top bar with drawer

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,12 @@
 import './bootstrap';
 
-import {createApp} from 'vue'
+import { createApp } from 'vue'
 
 import App from '../Components/App.vue'
+import Navbar from '../Components/Layouts/Navbar.vue'
 
-createApp(App).mount("#app")
+const app = createApp(App)
+
+app.component('navbar', Navbar)
+
+app.mount('#app')
